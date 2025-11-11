12464c0e71a645608f6425c0b87d43f3be254525
obadah <obadahesa881@gmail.com>
2025-11-11 16:49:03 +0200

    adding last login method

--- src/auth/plugin.ts
+++ src/auth/plugin.ts
@@ -1,6 +1,7 @@
 import { createAuthClient } from "better-auth/client";
 import { usernameClient } from "better-auth/client/plugins";
+import { lastLoginMethodClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  plugins: [usernameClient()],
+  plugins: [usernameClient(), lastLoginMethodClient()],
 });
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
-import { username } from "better-auth/plugins"
+import { username } from "better-auth/plugins";
+import { lastLoginMethod } from "better-auth/plugins";
 
 const prisma = new PrismaClient();
 
@@ -9,14 +10,17 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-  //   jwt: {
-  //     secret: process.env.JWT_SECRET!,
-  //   },
+
   emailAndPassword: { enabled: true },
   cors: {
     origin: "*",
     credentials: true,
   },
   trustedOrigins: ["*"],
-  plugins: [ username() ],
+  plugins: [
+    username(),
+    lastLoginMethod({
+      storeInDatabase: true,
+    }),
+  ],
 });
